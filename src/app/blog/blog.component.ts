f1a2c1cf7a5ef6adb7fcd2d55786068a52fca713
Manish Gautam <manish.gautam.india@gmail.com>
2024-03-22 01:21:02 +0530

    updates on blog API

--- src/app/blog/blog.component.ts
+++ src/app/blog/blog.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BlogService } from '../services/blog.service';
 
 @Component({
   selector: 'app-blog',
-  standalone: true,
-  imports: [],
   templateUrl: './blog.component.html',
-  styleUrl: './blog.component.scss'
+  styleUrls: ['./blog.component.scss']
 })
-export class BlogComponent {
+export class BlogComponent implements OnInit {
+  posts: any[] = [];
 
+  constructor(private blogService: BlogService) { }
+
+  async ngOnInit() {
+    this.blogService.fetchPosts().then((resp:any) => {
+      debugger
+      console.log(resp);
+      this.posts = resp?.items;
+
+      console.log("************", resp?.items[0]?.data?.title?.iv)
+    })
+    .catch((err: any) => {
+      console.log(err)
+    })
+  }
 }
