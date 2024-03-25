47a2d159e35f4800658c38d6e1da8f1a5243fe51
Manish Gautam <manish.gautam.india@gmail.com>
2024-03-25 10:10:05 +0530

    Updates on Blog page

--- src/app/blogs/blogs.component.ts
+++ src/app/blogs/blogs.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { BlogService } from '../services/blog.service';
 
 @Component({
   selector: 'app-blogs',
@@ -8,5 +9,19 @@ import { Component } from '@angular/core';
   styleUrl: './blogs.component.scss'
 })
 export class BlogsComponent {
+  posts: any[] = [];
 
+  constructor(private blogService: BlogService) { }
+
+  async ngOnInit() {
+    this.blogService.fetchPosts().then((resp:any) => {
+      console.log(resp);
+      this.posts = resp?.items;
+
+      console.log("************", resp?.items[0]?.data?.title?.iv)
+    })
+    .catch((err: any) => {
+      console.log(err)
+    })
+  }
 }
