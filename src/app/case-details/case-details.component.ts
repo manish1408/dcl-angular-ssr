a3e32f6706c79f20524b9190cbe46e4efaa03c1d
Manish Gautam <manish.gautam.india@gmail.com>
2024-03-27 11:45:58 +0530

    case-study-details content filled

--- src/app/case-details/case-details.component.ts
+++ src/app/case-details/case-details.component.ts
@@ -1,36 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 import { CaseStudyService } from '../services/case-study.service';
 import { ActivatedRoute } from '@angular/router';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-case-details',
   standalone: true,
   imports: [],
   templateUrl: './case-details.component.html',
-  styleUrl: './case-details.component.scss'
+  styleUrl: './case-details.component.scss',
 })
 export class CaseDetailsComponent implements OnInit {
-
-  constructor(private caseStudyService: CaseStudyService, private route: ActivatedRoute) { }
-  posts: any[] = [];
-  slugName: string=''
+  constructor(
+    private caseStudyService: CaseStudyService,
+    private route: ActivatedRoute
+  ) {}
+  post: any;
+  slugName: string = '';
+  imgCDN: string = environment.squidexAssets;
 
   async ngOnInit() {
-
-    this.route.params.subscribe(param => {
+    this.route.params.subscribe((param) => {
       this.slugName = param['type'];
-      console.log(this.slugName); 
+      console.log(this.slugName);
     });
-    this.caseStudyService.getCaseStudyBySlug(this.slugName).then((resp:any) => {
-    
-      console.log(resp);
-      this.posts = resp?.items;
+    this.caseStudyService
+      .getCaseStudyBySlug(this.slugName)
+      .then((resp: any) => {
+        console.log(resp);
+        this.post = resp?.items[0].data;
 
-      console.log("************", resp)
-    })
-    .catch((err: any) => {
-      console.log(err)
-    })
+        console.log('************', this.post);
+      })
+      .catch((err: any) => {
+        console.log(err);
+      });
   }
-
 }
