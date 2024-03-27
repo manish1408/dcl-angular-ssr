b13d2853773ac107cf21a991cdd6b9b3252f7c6e
Manish Gautam <manish.gautam.india@gmail.com>
2024-03-27 12:31:57 +0530

    updates on case-details, adding loader

--- src/app/case-details/case-details.component.ts
+++ src/app/case-details/case-details.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { CaseStudyService } from '../services/case-study.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, RouterModule, RouterOutlet } from '@angular/router';
 import { environment } from '../environments/environment';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-case-details',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, RouterOutlet, CommonModule],
   templateUrl: './case-details.component.html',
   styleUrl: './case-details.component.scss',
 })
@@ -16,21 +17,42 @@ export class CaseDetailsComponent implements OnInit {
     private route: ActivatedRoute
   ) {}
   post: any;
+  posts: any = [];
   slugName: string = '';
   imgCDN: string = environment.squidexAssets;
+  isLoading: boolean = true;
 
   async ngOnInit() {
     this.route.params.subscribe((param) => {
       this.slugName = param['type'];
       console.log(this.slugName);
+
+      this.caseStudyService
+        .getCaseStudyBySlug(this.slugName)
+        .then((resp: any) => {
+          console.log(resp);
+          this.post = resp?.items[0].data;
+
+          console.log('************', this.post);
+          this.getAllCaseStudies();
+        })
+        .catch((err: any) => {
+          console.log(err);
+        });
     });
+  }
+
+  getAllCaseStudies() {
     this.caseStudyService
-      .getCaseStudyBySlug(this.slugName)
+      .fetchPosts()
       .then((resp: any) => {
         console.log(resp);
-        this.post = resp?.items[0].data;
 
-        console.log('************', this.post);
+        this.posts = resp?.items.filter((item: any) => {
+          return item.data.slug.iv !== this.slugName;
+        });
+
+        console.log('All case studies:', this.posts);
       })
       .catch((err: any) => {
         console.log(err);
