b893aadfa5af9c1dcdc7c0f5940999e092fa5880
Manish Gautam <manish.gautam.india@gmail.com>
2024-03-29 15:07:01 +0530

    thankyo page main section complete

--- src/app/schedule-call/thank-you/thank-you.component.ts
+++ src/app/schedule-call/thank-you/thank-you.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-thank-you',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './thank-you.component.html',
-  styleUrl: './thank-you.component.scss'
+  styleUrl: './thank-you.component.scss',
 })
-export class ThankYouComponent {
+export class ThankYouComponent implements OnInit {
+  constructor(private router: Router) {}
+  countdown: number = 10;
+  countdownText: string = `${this.countdown} seconds`;
+  ngOnInit(): void {
+    this.startCountdown();
+  }
 
+  startCountdown(): void {
+    const countdownInterval = setInterval(() => {
+      this.countdown--;
+      this.countdownText = `${this.countdown} second${
+        this.countdown !== 1 ? 's' : ''
+      }`;
+
+      if (this.countdown <= 0) {
+        clearInterval(countdownInterval);
+        this.router.navigate(['/blog']);
+      }
+    }, 1000);
+  }
 }
