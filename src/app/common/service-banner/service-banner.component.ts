3c789c9318c0964375c3aa8421e3c134ba7a1d97
Manish Gautam <manish.gautam.india@gmail.com>
2024-04-02 17:50:10 +0530

    submit functionality on shared form

--- src/app/common/service-banner/service-banner.component.ts
+++ src/app/common/service-banner/service-banner.component.ts
@@ -1,11 +1,14 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import {
   FormBuilder,
   FormGroup,
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
+import { FormDataService } from '../../services/form-data.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-service-banner',
@@ -15,10 +18,17 @@ import {
   styleUrl: './service-banner.component.scss',
 })
 export class ServiceBannerComponent implements OnInit {
+  @Output() submit: EventEmitter<void> = new EventEmitter<void>();
   isLoading: boolean = false;
-  basicDetailsForm!: FormGroup;
-  constructor(private fb: FormBuilder) {
-    this.basicDetailsForm = this.fb.group({
+  detailsForm!: FormGroup;
+  id!: string;
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private formDataService: FormDataService,
+    private toastr: ToastrService
+  ) {
+    this.detailsForm = this.fb.group({
       name: ['', [Validators.required]],
       email: ['', [Validators.required, Validators.email]],
       serviceMessage: ['', [Validators.required]],
@@ -26,15 +36,35 @@ export class ServiceBannerComponent implements OnInit {
   }
 
   ngOnInit(): void {}
-  hasError(controlName: keyof typeof this.basicDetailsForm.controls) {
-    const control = this.basicDetailsForm.controls[controlName];
+  hasError(controlName: keyof typeof this.detailsForm.controls) {
+    const control = this.detailsForm.controls[controlName];
     return control.invalid && control.touched;
   }
   hasEmailFormatError() {
     return (
-      this.basicDetailsForm.controls['email'].hasError('email') &&
-      this.basicDetailsForm.controls['email'].touched
+      this.detailsForm.controls['email'].hasError('email') &&
+      this.detailsForm.controls['email'].touched
     );
   }
-  onSubmit() {}
+  onSubmit() {
+    if (this.detailsForm.valid) {
+      this.submit.emit(this.detailsForm.value);
+      this.formDataService
+        .saveScheduleCall(this.detailsForm.value)
+        .subscribe((res) => {
+          console.log(res);
+          if (res.result === 1) {
+            this.id = res.data._id;
+            this.router.navigate([
+              '/schedule-call/contact-information',
+              this.id,
+            ]);
+          }
+        }),
+        (error: any) => {
+          console.error('Error occurred:', error);
+          this.toastr.error('Something went wrong. Please try again.');
+        };
+    }
+  }
 }
