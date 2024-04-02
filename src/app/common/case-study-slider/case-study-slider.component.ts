87208ca91a1311ff0a92c92534f0c6d8c8d1fc4a
Manish Gautam <manish.gautam.india@gmail.com>
2024-04-02 14:53:56 +0530

    add Swiper config to case-study-slider

--- src/app/common/case-study-slider/case-study-slider.component.ts
+++ src/app/common/case-study-slider/case-study-slider.component.ts
@@ -1,6 +1,13 @@
 import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  Input,
+  Output,
+} from '@angular/core';
 import { RouterModule } from '@angular/router';
+declare var Swiper: any;
 
 @Component({
   selector: 'app-case-study-slider',
@@ -15,13 +22,49 @@ export class CaseStudySliderComponent {
   @Output() nextClick: EventEmitter<void> = new EventEmitter<void>();
   @Output() prevClick: EventEmitter<void> = new EventEmitter<void>();
 
-  constructor() {}
+  constructor(private elementRef: ElementRef) {}
+  currentIndex: number = 0;
 
   swiperNext(): void {
-    this.nextClick.emit();
+    // this.nextClick.emit();
+    if (this.currentIndex < this.currentCaseStudy.length - 1) {
+      this.currentIndex++;
+      console.log(this.currentCaseStudy[this.currentIndex]);
+    }
   }
 
   swiperPrev(): void {
-    this.prevClick.emit();
+    // this.prevClick.emit();
+    if (this.currentIndex > 0) {
+      this.currentIndex--;
+      console.log(this.currentCaseStudy[this.currentIndex]);
+    }
+  }
+
+  ngAfterViewInit() {
+    window.setTimeout(() => {
+      var swiper = new Swiper(
+        this.elementRef.nativeElement.querySelector('.case-study-slider'),
+        {
+          slidesPerView: 1,
+          speed: 1500,
+          spaceBetween: 30,
+          loop: true,
+          navigation: {
+            nextEl: '.case-study-slider-next',
+            prevEl: '.case-study-slider-prev',
+          },
+          breakpoints: {
+            280: { slidesPerView: 1 },
+            386: { slidesPerView: 1 },
+            576: { slidesPerView: 1, spaceBetween: 15 },
+            768: { slidesPerView: 2, spaceBetween: 15 },
+            992: { slidesPerView: 1, spaceBetween: 20 },
+            1200: { slidesPerView: 2, spaceBetween: 15 },
+            1400: { slidesPerView: 2 },
+          },
+        }
+      );
+    }, 500);
   }
 }
