11d87aa65d536ec19e9057cc63cd1559e2ec8ca5
Manish Gautam <manish.gautam.india@gmail.com>
2024-04-02 16:23:13 +0530

    Updates

---
+++ src/app/common/solutions-card/solutions-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-solutions-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './solutions-card.component.html',
+  styleUrl: './solutions-card.component.scss'
+})
+export class SolutionsCardComponent {
+
+}
