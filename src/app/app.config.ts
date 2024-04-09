99ca62d652d04b736f2b2e9beeaf7bb4098d2ed8
Anwarul Islam <anwaarulislaam@gmail.com>
2024-04-09 15:37:19 +0600

    feat: ssr packages added

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -17,6 +17,6 @@ export const appConfig: ApplicationConfig = {
       preventDuplicates: true,
     }),
     provideAnimations(),
-    importProvidersFrom(HttpClientModule),
+    importProvidersFrom(HttpClientModule), provideClientHydration(),
   ],
 };
