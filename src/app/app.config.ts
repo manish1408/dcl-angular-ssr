a4af5692642dc6ab915ccaa80c2853f09b03e7de
Anwarul Islam <anwaarulislaam@gmail.com>
2024-04-11 01:48:59 +0600

    feat: add http with fetch

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,22 +1,28 @@
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
-import { routes } from './app.routes';
+import {
+  HttpClientModule,
+  provideHttpClient,
+  withFetch,
+} from '@angular/common/http';
 import { provideClientHydration } from '@angular/platform-browser';
-import { provideToastr } from 'ngx-toastr';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { provideToastr } from 'ngx-toastr';
+import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideClientHydration(),
+    provideHttpClient(withFetch()),
     provideToastr({
       timeOut: 3000,
       positionClass: 'toast-top-right',
       preventDuplicates: true,
     }),
     provideAnimations(),
-    importProvidersFrom(HttpClientModule), provideClientHydration(),
+    importProvidersFrom(HttpClientModule),
+    provideClientHydration(),
   ],
 };
