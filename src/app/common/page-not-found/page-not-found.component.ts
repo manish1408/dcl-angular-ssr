2234239cd4b86f917ef351a3a08a7d18537729bf
Manish Gautam <manish.gautam.india@gmail.com>
2024-04-26 12:11:04 +0530

    add page-not-found page

---
+++ src/app/common/page-not-found/page-not-found.component.ts
@@ -0,0 +1,12 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-page-not-found',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './page-not-found.component.html',
+  styleUrl: './page-not-found.component.scss',
+  encapsulation: ViewEncapsulation.None,
+})
+export class PageNotFoundComponent {}
