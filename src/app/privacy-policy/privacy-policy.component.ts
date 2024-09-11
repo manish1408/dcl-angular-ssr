00448b5faa3d604a1e117162a62e4c701e9bfcb1
Manish Gautam <manish.gautam.india@gmail.com>
2024-09-11 11:25:23 +0530

    Updates on Privacy Policy

--- src/app/privacy-policy/privacy-policy.component.ts
+++ src/app/privacy-policy/privacy-policy.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
+import { BlogService } from '../services/blog.service';
+import { CommonModule, DatePipe } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-privacy-policy',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, CommonModule],
   templateUrl: './privacy-policy.component.html',
   styleUrl: './privacy-policy.component.scss'
 })
 export class PrivacyPolicyComponent {
 
+  constructor(
+    private el: ElementRef
+  ) {}
+  scrollToSection() {
+    const section = this.el.nativeElement.querySelector('#blog-grid');
+    if (section) {
+      section.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
 }
